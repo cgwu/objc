19206bd7df6e69abc627cda54266c8cdb9ccbdaa
test5d <test5d@userdeMacBook-Pro.local>
2015-02-08 20:36:15 +0800

    2015

---
+++ 2015/HelloSwift/HelloSwift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by test5d on 15/2/8.
+//  Copyright (c) 2015年 test5d. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+println("Hello中国")
+println(1+1)
