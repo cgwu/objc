7b50642c86a7492e4b0db22a33470bcb66153f81
test5d <test5d@userdeMacBook-Pro.local>
2015-02-09 00:32:21 +0800

    声明对象复习，属性。

--- TestProject/Fraction/main+hello.m
+++ TestProject/Fraction/main+hello.m
@@ -10,10 +10,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printHello(){
+    NSLog(@"Programming is fun.");
+}
+
+@interface Hello : NSObject
+@property (strong,nonatomic, getter=fullname) NSString *name;
+@property int age;
+@end
+
+@implementation Hello
+@synthesize name,age;
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"HelloWorld");
+        printHello();
+        Hello *ph = [[Hello alloc]init];
+        ph.name = @"张三";
+        ph.age = 110;
+        NSLog(@"ph.name=%@,age=%d",ph.fullname,ph.age);
+        
     }
     printf("Hello中国\n");
     return 0;
 }
+
